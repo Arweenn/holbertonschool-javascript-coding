b8578c7fbea923444bc975102a9fee7085445482
Arweenn <arwendumont25@outlook.fr>
2024-05-13 13:25:02 +0200

    First commit for the task 3 - Star wars movie title

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movieId = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+request(movieId, (err, response, body) => {
+  if (err){
+    console.error(err);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
