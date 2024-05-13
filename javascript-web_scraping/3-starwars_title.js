939802af2a095ecbf10efb7518b510fe204e9e25
Arweenn <arwendumont25@outlook.fr>
2024-05-13 13:25:56 +0200

    Syntax

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -4,7 +4,7 @@ const request = require('request');
 const movieId = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
 request(movieId, (err, response, body) => {
-  if (err){
+  if (err) {
     console.error(err);
   } else {
     console.log(JSON.parse(body).title);
