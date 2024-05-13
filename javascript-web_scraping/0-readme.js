7d6189ac798053cb533fb126fe43e7f2a3a08e3e
Arweenn <arwendumont25@outlook.fr>
2024-05-13 10:36:47 +0200

    First commit for the task 0 - Readme

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const file = process.argv[2];
+
+fs.readFile(file, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
