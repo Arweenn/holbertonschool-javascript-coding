600b964d38b69558bdb027392ee2e22c576f78c0
Arweenn <arwendumont25@outlook.fr>
2024-05-13 15:43:35 +0200

    First commit for the task 6 - How many completed?

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const tasks = {};
+    for (const task of JSON.parse(body)) {
+      if (task.completed === true) {
+        if (tasks[task.userId] === undefined) {
+          tasks[task.userId] = 1;
+        } else {
+          tasks[task.userId] += 1;
+        }
+      }
+    }
+    console.log(tasks);
+  }
+});
