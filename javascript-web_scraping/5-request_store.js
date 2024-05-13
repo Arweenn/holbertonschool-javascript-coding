3c08bc32dd47529f5f6349af2c594b627aa6d17e
Arweenn <arwendumont25@outlook.fr>
2024-05-13 15:20:46 +0200

    First commit for the task 5 - Loripsum

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const request = require('request')
+const url = process.argv[2];
+const file = process.argv[3];
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(file, body, 'utf-8', (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  }
+})
+
