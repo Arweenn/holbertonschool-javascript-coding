c9b63ce4877bab7e2b4d8bbf780301fde854a43a
Arweenn <arwendumont25@outlook.fr>
2024-05-13 15:22:26 +0200

    Syntax fix

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const request = require('request')
+const request = require('request');
 const url = process.argv[2];
 const file = process.argv[3];
 
@@ -15,5 +15,4 @@ request(url, (err, response, body) => {
       }
     });
   }
-})
-
+});
