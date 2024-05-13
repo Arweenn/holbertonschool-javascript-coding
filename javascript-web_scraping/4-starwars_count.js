b606b79868f0a5761e3a473c7a08c516cef415ef
Arweenn <arwendumont25@outlook.fr>
2024-05-13 15:01:51 +0200

    First commit for the task 4 - Star wars Wedge Antilles

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+const charId = 18;
+let count = 0;
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    for (const movie of JSON.parse(body).results) {
+      for (const character of movie.characters) {
+        if (character.includes('18')) {
+          count += 1;
+        }
+      }
+    }
+  }
+  console.log(count);
+});
