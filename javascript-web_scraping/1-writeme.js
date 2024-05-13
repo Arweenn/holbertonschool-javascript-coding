0890298ebaf4a9bc3d150bee3520cddc1ff3fbba
Arweenn <arwendumont25@outlook.fr>
2024-05-13 11:57:46 +0200

    First commit for the task 1 - Write me

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const file = process.argv[2];
+const text = process.argv[3];
+
+fs.writeFile(file, text, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
